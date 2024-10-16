c303986adcd72d75e479e5bfb439b45efd3feb48
jb101010-2 <dev.julien.basson@gmail.com>
2024-10-16 12:08:37 +0200

    feat: Fetch historical consumption data

--- homeassistant/components/suez_water/coordinator.py
+++ homeassistant/components/suez_water/coordinator.py
@@ -1,19 +1,29 @@
 """Suez water update coordinator."""
 
 import asyncio
-from datetime import datetime, timedelta
-from typing import Any
+from datetime import date, datetime, time, timedelta, timezone
+from typing import Any, cast
 
 from pysuez import SuezData
 from pysuez.async_client import SuezAsyncClient
 from pysuez.client import PySuezError
 from pysuez.suez_data import AlertResult, ConsumptionIndexResult, DayDataResult
+import pytz
 
+from homeassistant.components.recorder import get_instance
+from homeassistant.components.recorder.models import StatisticData, StatisticMetaData
+from homeassistant.components.recorder.statistics import (
+    StatisticsRow,
+    async_add_external_statistics,
+    get_last_statistics,
+)
+from homeassistant.const import UnitOfVolume
 from homeassistant.core import _LOGGER, HomeAssistant
 from homeassistant.exceptions import ConfigEntryError
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import DOMAIN
+from ..toon.const import CURRENCY_EUR
 
 
 class SuezWaterCoordinator(DataUpdateCoordinator):
@@ -30,8 +40,8 @@ class SuezWaterCoordinator(DataUpdateCoordinator):
         super().__init__(
             hass,
             _LOGGER,
-            name=f"{DOMAIN}_{counter_id}",
-            update_interval=timedelta(minutes=3),
+            name=DOMAIN,
+            update_interval=timedelta(hours=12),
             always_update=True,
         )
         self._async_client = async_client
@@ -40,7 +50,9 @@ class SuezWaterCoordinator(DataUpdateCoordinator):
         self._price: None | float = None
         self.alerts: None | AlertResult = None
         self.index: None | ConsumptionIndexResult = None
-        _LOGGER.debug("Creating coordinator")
+        self._statistic_id = f"{DOMAIN}:{counter_id}_water_consumption_v2"
+        self.config_entry.async_on_unload(self._clear_statistics)
+        _LOGGER.debug("Created coordinator")
 
     async def _async_setup(self) -> None:
         """Set up the coordinator."""
@@ -62,13 +74,14 @@ class SuezWaterCoordinator(DataUpdateCoordinator):
     async def _async_update_data(self) -> dict[str, Any]:
         """Fetch data from API endpoint."""
         try:
-            async with asyncio.timeout(60):
+            async with asyncio.timeout(200):
                 await self._fetch_last_day_consumption_data()
                 await self._fetch_consumption_index()
                 await self._fetch_price()
                 await self._fetch_alerts()
+                await self._update_historical()
                 await self._async_client.close_session()
-                _LOGGER.info("Suez update completed")
+                _LOGGER.warning("Suez update completed")
                 return {"update": datetime.now()}
         except PySuezError as err:
             raise UpdateFailed(
@@ -98,6 +111,99 @@ class SuezWaterCoordinator(DataUpdateCoordinator):
 
     async def _fetch_alerts(self) -> None:
         self.alerts = await self._data_api.get_alerts()
+
+    async def _update_historical(self) -> None:
+        _LOGGER.warning("Updating statistics for %s", self._statistic_id)
+        cost_stat_id = self._statistic_id + "_cost"
+
+        last_stat = await get_instance(self.hass).async_add_executor_job(
+                get_last_statistics, self.hass, 1, self._statistic_id, True, {"sum"}
+            )
+        cost_last_stat = await get_instance(self.hass).async_add_executor_job(
+                get_last_statistics, self.hass, 1, cost_stat_id, True, {"sum"}
+            )
+        usage: list[DayDataResult]
+        try:
+            last_stats_time: date | None
+            if last_stat is None or len(last_stat) == 0:
+                    _LOGGER.debug("Updating suez statistic for the first time")
+                    usage = await self._data_api.fetch_all_available()
+                    consumption_sum = 0.0
+                    cost_sum = 0.0
+                    last_stats_time = None
+            else:
+                    previous_stat: StatisticsRow = last_stat[self._statistic_id][0]
+                    previous_cost_stat: StatisticsRow = cost_last_stat[cost_stat_id][0]
+                    last_stats_time = datetime.fromtimestamp(previous_stat["start"]).date()
+                    _LOGGER.debug("Updating suez stat since %s for %s", str(last_stats_time), previous_stat)
+                    usage = await self._data_api.fetch_all_available(
+                        since=last_stats_time,
+                    )
+                    if usage is None or len(usage) <= 0:
+                        _LOGGER.debug("No recent usage data. Skipping update")
+                        return
+                    consumption_sum = cast(float, previous_stat["sum"])
+                    cost_sum = cast(float, previous_cost_stat["sum"])
+        except Exception as err:
+            _LOGGER.error("Error while fetching historical data, %s", err)
+            raise err
+        _LOGGER.debug("fetched data: %s", len(usage))
+
+        consumption_statistics = []
+        cost_statistics = []
+
+        for data in usage:
+            if last_stats_time is not None and data.date <= last_stats_time:
+                continue
+            consumption_sum += data.day_consumption
+            consumption_statistics.append(
+                StatisticData(
+                    start=datetime.combine(data.date, time(0,0,0,0), pytz.timezone('Europe/Paris')), state=data.day_consumption, sum=consumption_sum
+                )
+            )
+            day_cost = (data.day_consumption / 1000) * self._price
+            cost_sum = cost_sum + day_cost
+            cost_statistics.append(
+                StatisticData(
+                    start=datetime.combine(data.date, time(0,0,0,0), pytz.timezone('Europe/Paris')), state=day_cost, sum=cost_sum
+                )
+            )
+
+        consumption_metadata = StatisticMetaData(
+            has_mean=False,
+            has_sum=True,
+            name=f"{self._statistic_id} Consumption",
+            source=DOMAIN,
+            statistic_id=self._statistic_id,
+            unit_of_measurement=UnitOfVolume.LITERS
+        )
+        cost_metadata = StatisticMetaData(
+            has_mean=False,
+            has_sum=True,
+            name=f"{self._statistic_id} Consumption cost",
+            source=DOMAIN,
+            statistic_id=cost_stat_id,
+            unit_of_measurement=CURRENCY_EUR + "/" + UnitOfVolume.LITERS
+        )
+
+        _LOGGER.trace(
+            "Adding %s statistics for %s",
+            len(consumption_statistics),
+            self._statistic_id,
+        )
+        async_add_external_statistics(
+            self.hass, consumption_metadata, consumption_statistics
+        )
+        async_add_external_statistics(
+            self.hass, cost_metadata, cost_statistics
+        )
+
+        _LOGGER.debug("Updated statistics for %s", self._statistic_id)
+
+
+    def _clear_statistics(self) -> None:
+        """Clear statistics."""
+        get_instance(self.hass).async_clear_statistics(list(self._statistic_id))
 
     def get_attribution(self) -> str:
         """Get attribution message."""
